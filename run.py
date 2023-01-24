b4834552aff48f4361b56e7386f17ce5470b4b9d
Amiay Narayan <35066387+amiaynara@users.noreply.github.com>
2023-01-25 02:22:40 +0530

    InitialCommit: Make the first draft of the letter-generator app

---
+++ run.py
@@ -0,0 +1,89 @@
+import json
+from flask import Flask, render_template, request
+import openai
+
+app = Flask(__name__)
+para1 = '''I hope this letter finds you well. I am writing to you today
+ to discuss a matter of importance regarding a financial
+ transaction that took place between us. Specifically, I
+am requesting that you pay back the sum of $500 to me.'''
+
+para2 = '''As you may recall, I loaned you this money on [date] with
+ the understanding that it would be paid back in a timely
+ manner. However, it has been [insert time frame] since
+ that loan and I have yet to receive any repayment from
+ you. I have reached out to you multiple times to inquire
+ about the status of the repayment, but have yet to
+ receive a response.'''
+
+para3 = '''I understand that financial difficulties can arise, but I
+ kindly ask that you make every effort to repay the
+ loan as soon as possible. I have faith that you will do
+ the right thing and pay back the money that I loaned
+ you. Please let me know your plan of action and
+ when I can expect to receive the money.\n\nYour sincerely,\n[Name]'''
+
+with open(r'/Users/amiaynarayan/Projects/credential.json') as config_file:
+    config = json.load(config_file)
+
+openai.api_key = config.get("OPENAI_API_KEY")
+
+def generate_letter(subject, number_of_paragraphs=3):
+    '''Method to return the first draft of letters'''
+    search_query = f'I want to write a letter about "{subject}". If I want the letter to be {number_of_paragraphs} paragraphs long, \
+        please write the letter.'
+    completions = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=search_query,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    print(completions)
+    letter = completions.choices[0].text
+    print(letter)
+    return letter
+
+def paragraph_letter(letter):
+    '''Break a letter into paragraphs'''
+    letter = letter.strip('\n')
+    letter_array = letter.split('\n\n')
+    paragraphed_letter = letter_array[1:len(letter_array)-1]
+    return paragraphed_letter
+
+@app.route('/choices')
+def choices():
+    return render_template('choices.html')
+
+@app.route('/parameters', methods=['GET', 'POST'])
+def parameters():
+    if request.method == 'POST':
+        # Remove the below line
+        # paras = f'\n\nDear [name],\n\n{para1}\n\n{para2}\n\n{para3}'
+        letter_type = request.form['Type']
+        num_paragraphs = request.form['number of paragraphs']
+        letter_generated = generate_letter(letter_type, num_paragraphs)
+        paragraphed_letter = paragraph_letter(letter_generated)
+        return render_template('edit.html', paragraphed_letter=paragraphed_letter)
+    return render_template('parameters.html')
+
+@app.route('/edit', methods=['GET', 'POST'])
+def edit():
+    if request.method == 'POST':
+        paragraphs = request.form.getlist('paragraph')
+        return render_template('finalise.html', paragraphs=paragraphs)
+
+@app.route('/finalise', methods=['GET', 'POST'])
+def finalise():
+    if request.method == 'POST':
+        letter = request.form['letter']
+        return render_template('letter.html', letter=letter)
+    return render_template('finalise.html')
+
+@app.route('/letter')
+def letter():
+    return render_template('letter.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
